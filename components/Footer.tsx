4a09b1c876a2f4f19c45b42b6aac3e221720e963
Abhishek-Shrivastava31 <shrivastava.abhishek3112@gmail.com>
2024-08-03 16:35:46 +0530

    footer final

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,23 +6,25 @@ const Footer = () => {
   return (
     <section className="w-[80vw] mt-20 mb-16">
       <div className="flex flex-col justify-between items-center sm:flex-row ">
-        <div className="text-slate-200">Copyright © 2024 Abhishek</div>
-        <div className="flex gap-6 text-xl ">
+        <div className="text-purple-100 text-sm sm:text-base">
+          Copyright © 2024 Abhishek
+        </div>
+        <div className="flex gap-6 mt-4 sm:mt-0 sm:text-xl text-purple-100">
           <Link
             href="#"
-            className="border border-slate-600 bg-slate-900/60 p-2 rounded-md"
+            className="border border-slate-600 bg-slate-900/60 p-1 sm:p-2 rounded-md"
           >
             <FaGithub />
           </Link>
           <Link
             href="#"
-            className="border border-slate-600 bg-slate-900/60 p-2 rounded-md"
+            className="border border-slate-600 bg-slate-900/60 p-1 sm:p-2 rounded-md"
           >
             <FaXTwitter />
           </Link>
           <Link
             href="#"
-            className="border border-slate-600 bg-slate-900/60 p-2 rounded-md"
+            className="border border-slate-600 bg-slate-900/60 p-1 sm:p-2 rounded-md"
           >
             <FaFacebook />
           </Link>
